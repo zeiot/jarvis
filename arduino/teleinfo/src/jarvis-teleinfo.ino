17e58f9f80bcc994121c18cbac403cf660f8fc5f
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2016-03-12 17:23:27 +0100

    Init MQTT for Arduino modules
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- arduino/teleinfo/src/jarvis-teleinfo.ino
+++ arduino/teleinfo/src/jarvis-teleinfo.ino
@@ -14,18 +14,90 @@
  * limitations under the License.
  */
 
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include <SoftwareSerial.h>
 
+/************************* Configuration *********************************/
+
+const char* ssid = "xxxxxxx";
+const char* password = "xxxxxxxxxxx";
+const char* mqtt_server = "192.10.10.2";
+const int mqtt_port = 8083;
+
+/************************* End configuration *****************************/
+
+
+WiFiClient wifiClient;
+PubSubClient mqttClient;
+
+
 SoftwareSerial cptSerial(2, 3);
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.printf("[Jarvis] Connecting to : %s\n", ssid);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.printf("[Jarvis] WiFi connected. IP: %s\n", WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.println("[Jarvis] Message arrived:  topic: " + String(topic));
+  Serial.println("[Jarvis] Length: " + String(length,DEC));
+  for(i=0; i<length; i++) {
+    message_buff[i] = payload[i];
+  }
+  message_buff[i] = '\0';
+  String msgString = String(message_buff);
+  Serial.println("[Jarvis] Payload: " + msgString);
+}
+
+void reconnect() {
+  while (!mqttClient.connected()) {
+    Serial.print("[Jarvis] Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqttClient.connect("ESP8266Client")) {
+      Serial.println("[Jarvis] connected");
+      mqttClient.publish("/jarvis/ping", "hello world from teleinfo");
+      // client.subscribe("");
+    } else {
+      Serial.print("[Jarvis] failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup_mqtt() {
+  mqttClient = PubSubClient(mqtt_server, mqtt_port, callback, wifiClient);
+  reconnect();
+
+}
+
 
 void setup() {
   Serial.begin(1200);
   cptSerial.begin(1200);
-  Serial.println("[Jarvis] TeleInfo");
+  setup_wifi();
+  setup_mqtt();
+  Serial.println("[Jarvis] TeleInfo ready");
+
 }
+
+char character;
 
 void loop() {
   if (cptSerial.available()) {
-    Serial.write(cptSerial.read() & 0x7F);
+    char teleinfo = cptSerial.read() & 0x7F;
+    client.publish("/jarvis/sensor/teleinfo", teleinfo);
   }
 }
