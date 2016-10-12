de684ffac0e2f3d29f593eaa2593561e61aba318
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2016-10-12 23:31:24 +0200

    Migrate to HypriotOS
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- sdcard/jarvis_os.sh
+++ sdcard/jarvis_os.sh
@@ -14,123 +14,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-RASPBIAN_VERSION=2016-09-28
-RASPBIAN_NUMBER=2016-09-23
+hostname=$1
+ssid=$2
+wifipassword=$3
+host=$4
 
 NO_COLOR="\033[0m"
 OK_COLOR="\033[32;01m"
 ERROR_COLOR="\033[31;01m"
 WARN_COLOR="\033[33;01m"
+DEBUG_COLOR="\033[34;01m"
 
+HYPRIOTOS_VERSION=1.0.0
 
-function display_usage() {
-    echo -e "${ERROR_COLOR}Usage: $0 <device> ${NO_COLOR}"
-    echo -e "With device:"
-    echo -e "  sdb for Linux"
-    echo -e "  rdisk1 for OSX"
-    exit 1
-}
-
-function get_os {
-    case "$OSTYPE" in
-        solaris*) echo "SOLARIS" ;;
-        darwin*)  echo "OSX" ;;
-        linux*)   echo "LINUX" ;;
-        bsd*)     echo "BSD" ;;
-        *)        echo "unknown: $OSTYPE" ;;
-    esac
-}
-
-
-function get_sdcard {
-    device=$1
-    os=$2
-    sdcard=""
-    case "${os}" in
-    "OSX")
-        sdcard="/dev/${device}"
-        ;;
-    "LINUX")
-        sdcard="/dev/${device}"
-        ;;
-    *)
-        echo -e "${ERROR_COLOR}Operating system not supported: ${os}${NO_COLOR}"
-        exit 1
-        ;;
-    esac
-    echo ${sdcard}
-}
-
-function setup_sdcard {
-    sdcard=$1
-    os=$2
-    case "${os}" in
-    "OSX")
-        echo -e "${WARN_COLOR}Unmounting${NO_COLOR}"
-        sudo diskutil unmountDisk ${sdcard}
-
-        echo -e "${WARN_COLOR}Setup SD Card${NO_COLOR}"
-        sudo dd if=/dev/zero of=${sdcard} bs=1024 count=1
-        ;;
-    "LINUX")
-        echo -e "${WARN_COLOR}Setup SD Card${NO_COLOR}"
-        parted -s ${sdcard} unit s print
-        parted ${sdcard} mkpart primary fat32 0 100%
-
-        echo -e "${WARN_COLOR}Unmounting${NO_COLOR}"
-        umount ${sdcard}1
-        ;;
-    *)
-        echo -e "${ERROR_COLOR}Operating system not supported: ${os}${NO_COLOR}"
-        ;;
-    esac
-}
-
-function flash_sdcard() {
-    sdcard=$1
-    image=$2
-    os=$3
-    case "${os}" in
-        "OSX")
-            sudo dd if=${image} of=${sdcard} bs=4m
-            sync
-            ;;
-        "LINUX")
-            sudo dd if=${image} of=${sdcard} bs=4M
-            sync
-            ;;
-        *)
-            echo -e "${ERROR_COLOR}Operating system not supported: ${os}${NO_COLOR}"
-            ;;
-    esac
-}
-
-
-
-echo -e "${OK_COLOR}== Raspbian Lite for Raspberry Pi ==${NO_COLOR}"
-if [ $# -ne 1 ]; then
-    display_usage
+echo -e "${OK_COLOR}== Jarvis OS: Hypriot ${HYPRIOTOS_VERSION} ==${NO_COLOR}"
+if [ $# -ne 4 ]; then
+  echo -e "${ERROR_COLOR}Usage: $0 hostname ssid wifipassword Linux|Darwin${NO_COLOR}"
+  exit 1
 fi
 
-os=$(get_os)
-echo -e "${WARN_COLOR}Operating system: $os${NO_COLOR}"
+echo -e "${DEBUG_COLOR}Download flash${NO_COLOR}"
+curl -LO --progress-bar https://raw.githubusercontent.com/hypriot/flash/master/${host}/flash
+chmod +x flash
+./flash --hostname ${hostname} --ssid ${ssid} --password ${wifipassword} https://downloads.hypriot.com/hypriotos-rpi-v${HYPRIOTOS_VERSION}.img.zip
 
-sdcard=$(get_sdcard $1 ${os})
-echo -e "${WARN_COLOR}Use sdcard :${NO_COLOR} ${sdcard}"
-
-echo -e "${WARN_COLOR}Downloading the root filesystem${NO_COLOR}"
-if [ ! -f "${RASPBIAN_NUMBER}-raspbian-jessie-lite.zip" ]; then
-    curl -LO --progress-bar http://director.downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-${RASPBIAN_VERSION}/${RASPBIAN_NUMBER}-raspbian-jessie-lite.zip
-fi
-
-echo -e "${WARN_COLOR}Extracting the image${NO_COLOR}"
-if [ ! -f "${RASPBIAN_NUMBER}-raspbian-jessie-lite.img" ]; then
-     unzip ${RASPBIAN_NUMBER}-raspbian-jessie-lite.zip
-fi
-
-setup_sdcard ${sdcard} ${os}
-
-echo -e "${WARN_COLOR}Installing image to SD Card${NO_COLOR}"
-flash_sdcard ${sdcard} ${RASPBIAN_NUMBER}-raspbian-jessie-lite.img ${os}
+echo -e "${DEBUG_COLOR}Cleanup${NO_COLOR}"
+rm ./flash
 
 echo -e "${OK_COLOR}== Done ==${NO_COLOR}"
