869767f029cbcc523f29561fd2e5949e16e0a2a3
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2016-10-12 23:10:54 +0200

    Update installation script
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- sdcard/jarvis_os.sh
+++ sdcard/jarvis_os.sh
@@ -14,10 +14,98 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-. commons.sh
-
 RASPBIAN_VERSION=2016-09-28
 RASPBIAN_NUMBER=2016-09-23
+
+NO_COLOR="\033[0m"
+OK_COLOR="\033[32;01m"
+ERROR_COLOR="\033[31;01m"
+WARN_COLOR="\033[33;01m"
+
+
+function display_usage() {
+    echo -e "${ERROR_COLOR}Usage: $0 <device> ${NO_COLOR}"
+    echo -e "With device:"
+    echo -e "  sdb for Linux"
+    echo -e "  rdisk1 for OSX"
+    exit 1
+}
+
+function get_os {
+    case "$OSTYPE" in
+        solaris*) echo "SOLARIS" ;;
+        darwin*)  echo "OSX" ;;
+        linux*)   echo "LINUX" ;;
+        bsd*)     echo "BSD" ;;
+        *)        echo "unknown: $OSTYPE" ;;
+    esac
+}
+
+
+function get_sdcard {
+    device=$1
+    os=$2
+    sdcard=""
+    case "${os}" in
+    "OSX")
+        sdcard="/dev/${device}"
+        ;;
+    "LINUX")
+        sdcard="/dev/${device}"
+        ;;
+    *)
+        echo -e "${ERROR_COLOR}Operating system not supported: ${os}${NO_COLOR}"
+        exit 1
+        ;;
+    esac
+    echo ${sdcard}
+}
+
+function setup_sdcard {
+    sdcard=$1
+    os=$2
+    case "${os}" in
+    "OSX")
+        echo -e "${WARN_COLOR}Unmounting${NO_COLOR}"
+        sudo diskutil unmountDisk ${sdcard}
+
+        echo -e "${WARN_COLOR}Setup SD Card${NO_COLOR}"
+        sudo dd if=/dev/zero of=${sdcard} bs=1024 count=1
+        ;;
+    "LINUX")
+        echo -e "${WARN_COLOR}Setup SD Card${NO_COLOR}"
+        parted -s ${sdcard} unit s print
+        parted ${sdcard} mkpart primary fat32 0 100%
+
+        echo -e "${WARN_COLOR}Unmounting${NO_COLOR}"
+        umount ${sdcard}1
+        ;;
+    *)
+        echo -e "${ERROR_COLOR}Operating system not supported: ${os}${NO_COLOR}"
+        ;;
+    esac
+}
+
+function flash_sdcard() {
+    sdcard=$1
+    image=$2
+    os=$3
+    case "${os}" in
+        "OSX")
+            sudo dd if=${image} of=${sdcard} bs=4m
+            sync
+            ;;
+        "LINUX")
+            sudo dd if=${image} of=${sdcard} bs=4M
+            sync
+            ;;
+        *)
+            echo -e "${ERROR_COLOR}Operating system not supported: ${os}${NO_COLOR}"
+            ;;
+    esac
+}
+
+
 
 echo -e "${OK_COLOR}== Raspbian Lite for Raspberry Pi ==${NO_COLOR}"
 if [ $# -ne 1 ]; then
@@ -31,7 +119,7 @@ sdcard=$(get_sdcard $1 ${os})
 echo -e "${WARN_COLOR}Use sdcard :${NO_COLOR} ${sdcard}"
 
 echo -e "${WARN_COLOR}Downloading the root filesystem${NO_COLOR}"
-if [ ! -f "${RASPBIAN_NUMVER}-raspbian-jessie-lite.zip" ]; then
+if [ ! -f "${RASPBIAN_NUMBER}-raspbian-jessie-lite.zip" ]; then
     curl -LO --progress-bar http://director.downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-${RASPBIAN_VERSION}/${RASPBIAN_NUMBER}-raspbian-jessie-lite.zip
 fi
 
@@ -40,9 +128,9 @@ if [ ! -f "${RASPBIAN_NUMBER}-raspbian-jessie-lite.img" ]; then
      unzip ${RASPBIAN_NUMBER}-raspbian-jessie-lite.zip
 fi
 
-setup_sdcard $(sdcard) $(os)
+setup_sdcard ${sdcard} ${os}
 
-echo -e "${WARN_COLOR}Installing OSMC to SD Card${NO_COLOR}"
-flash_sdcard ${sdcard} OSMC_TGT_rpb2_${osmc_img_version}.img ${os}
+echo -e "${WARN_COLOR}Installing image to SD Card${NO_COLOR}"
+flash_sdcard ${sdcard} ${RASPBIAN_NUMBER}-raspbian-jessie-lite.img ${os}
 
 echo -e "${OK_COLOR}== Done ==${NO_COLOR}"
