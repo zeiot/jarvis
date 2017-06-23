83218e66fc05d47f7026866084adfccd0c6a7ea3
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2017-06-23 15:11:27 +0200

    Cleanup
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- sdcard/jarvis_k8s.sh
+++ sdcard/jarvis_k8s.sh
@@ -14,21 +14,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-K8S_ARM_VERSION=0.8.0
-
 NO_COLOR="\033[0m"
 OK_COLOR="\033[32;01m"
 ERROR_COLOR="\033[31;01m"
 WARN_COLOR="\033[33;01m"
 DEBUG_COLOR="\033[34;01m"
 
+K8S_VERSION="v1.4.4"
+
+if [ $# -eq 0 ]; then
+    echo -e "${ERROR_COLOR}Usage: $0 <MASTER_IP>${NO_COLOR}"
+    exit 1
+fi
+K8S_MASTER_IP=$1
+
 echo -e "${OK_COLOR}== Jarvis Kubernetes ${K8S_ARM_VERSION}==${NO_COLOR}"
 
-echo -e "${DEBUG_COLOR}Download kubernetes-on-arm${NO_COLOR}"
-curl -LO --progress-bar https://github.com/luxas/kubernetes-on-arm/releases/download/v${K8S_ARM_VERSION}/docker-multinode.deb
-sudo dpkg -i docker-multinode.deb
+echo -e "${OK_COLOR}== Setup Kubernetes ==${NO_COLOR}"
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get -y kubelet kubeadm kubectl kubernetes-cni
 
-echo -e "${DEBUG_COLOR}Cleanup${NO_COLOR}"
-rm docker-multinode.deb
+echo -e "${OK_COLOR} == Start Kubelet ==${NO_COLOR}"
+sudo systemctl enable kubelet && sudo systemctl start kubelet
+
+echo -e "${OK_COLOR} == Initiliaze master ==${NO_COLOR}"
+kubeadm init --use-kubernetes-version ${K8S_VERSION} --api-advertise-addresses=${K8S_MASTER_IP} --pod-network-cidr=10.244.0.0/16
 
 echo -e "${OK_COLOR}== Done ==${NO_COLOR}"
